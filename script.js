7e44733e71d720531b48f55966054fec4a3c735e
Alexander Huang <ahuangliu@ucsd.edu>
2025-11-16 22:16:17 -0800

    Updated the forms structure, and added the base for extra credit customizable theme picker.

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const toggleBtn = document.getElementById('theme-toggle');
+const lightDarkBtn = document.getElementById('light-dark');
+const pickThemeBtn = document.getElementById('theme-pick');
+const themeForm = document.getElementById('theme-form')
 const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme === 'dark') {
     document.documentElement.setAttribute('data-theme', 'dark');
-    toggleBtn.textContent = 'Dark';
 }
 
-toggleBtn.addEventListener('click', () => {
+toggleBtn.addEventListener('click', pickTheme);
+lightDarkBtn.addEventListener('click', changeLDTheme);
+
+
+function pickTheme() {
+    themeForm.classList.toggle('hidden');
+}
+
+function changeLDTheme() {
     const isDark = document.documentElement.getAttribute('data-theme') === 'dark';
     document.documentElement.setAttribute('data-theme', isDark ? 'light' : 'dark');
-    toggleBtn.textContent = isDark ? 'Light' : 'Dark';
+    lightDarkBtn.textContent = isDark ? 'Light' : 'Dark';
     localStorage.setItem('theme', isDark ? 'light' : 'dark');
-});
+}
